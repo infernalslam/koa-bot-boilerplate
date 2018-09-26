3a9f2139a554a2c968ba4da6df5e24c0a8210d04
Kamonpob <kamonpob@TH-LW11327-m.local>
2018-09-26 15:34:39 +0700

    fix code

--- src/controllers/bot.controller.js
+++ src/controllers/bot.controller.js
@@ -1,5 +1,9 @@
 const botController = {
   async webhook (ctx) {
+    const koaRequest = ctx.request
+    console.log('koaRequest', koaRequest)
+    const requestEvents = koaRequest.body.events
+    console.log('requestEvents', requestEvents)
     ctx.status = 200
     ctx.body = ''
   }
